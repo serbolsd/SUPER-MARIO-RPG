93fb4baae143322f5778b6f1f51840a37d0885c4
Macrochaos <andressumano@hotmail.com>
2019-06-20 18:17:31 -0500

    temp commit for pulls

--- SUPER MARIO RPG/Assets/Scripts/Utilities/gUtilities.cs
+++ SUPER MARIO RPG/Assets/Scripts/Utilities/gUtilities.cs
@@ -6,8 +6,7 @@ public class gUtilities
 {
     public const float kHORIZONTAL_DIR = 0.88205f;
     public const float kVERTICAL_DIR = 0.43800142f;
-
-    const float stickDeadZone = 0.2f;
+    public const float kSTICKDEADZONE = 0.2f;
 
     int Attack(cCharacter _char, cCharacter _target, float timingMod)
     {
