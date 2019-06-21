6fef84786028a840d684aca967a223342af5de16
serbolsd <32022302+serbolsd@users.noreply.github.com>
2019-06-21 01:02:24 -0500

    prefab de puerta de castillo y prefab de cambio de scena

--- SUPER MARIO RPG/Assets/Scripts/INICIO/SALTAR.cs
+++ SUPER MARIO RPG/Assets/Scripts/INICIO/SALTAR.cs
@@ -22,6 +22,11 @@ public class SALTAR : MonoBehaviour
         {
             SceneManager.LoadScene("PRESSTART");
         }
+        else if(timeTrans>332)
+        {
+            SceneManager.LoadScene("PRESSTART");
+
+        }
 
     }
 }
